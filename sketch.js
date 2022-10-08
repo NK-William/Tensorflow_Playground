b84126e6d2b98a1fc8d6cb9516a6db967a817471
Nkuna William <64930245+NK-William@users.noreply.github.com>
2022-10-08 21:51:28 +0200

    TensorFlow.js: Variables & Operations - Intelligence and Learning

--- sketch.js
+++ sketch.js
@@ -5,13 +5,27 @@ function setup(){
     //data.print();
     //console.log(data.toString());
     //console.log(data);
-    
-
-    const values = [1,2,3,4,5,6,7,8,9,1,2,3,4,5,6];
-
-const shape = [5, 3];
-const data = tf.tensor(values, shape);
-console.log(data.toString()); 
 }
 
-setup(); 
+// setup(); 
+
+const v = [];
+for(i = 0; i < 30; i++){
+    v[i] = Math.random() * 100;
+}
+
+const shape = [2, 5, 3];
+
+// Note: "v" containing the array is store in the computer memory, and when passing the v to tensor32 -> the array is then stored
+// in the comnputer's GPU.
+const ts = tf.tensor3d(v, shape, 'int32'); // the value of "ts" is immutable.
+
+const vts = tf.variable(ts); // converting a tensor to a variable to mutate it("vts" is mutable)
+console.log(vts);
+
+// console.log(ts.toString());
+ //ts.print();
+//  data returns a promise, because of data being retrieved from GPU.
+// console.log(ts.dataSync());
+
+
